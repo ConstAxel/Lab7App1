776d712bd36186e33f23c0b645dcd26a0f77d55b
ConstAxel <ConstAxel@CONSTAXEL>
2021-10-14 22:36:09 +0300

    Добавьте файлы проекта.

---
+++ Lab7App1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7App1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //            Два треугольника заданы длинами своих сторон. Определить, площадь какого из них больше(создать метод для вычисления  площади  треугольника по длинам его сторон).Для решения задачи можно использовать формулу Герона
+
+            //, где x, y, z – стороны треугольника, p – полупериметр.
+            double a, b, c;
+            double P, S;
+            Console.WriteLine("Введите длины сторон первого треугольника");
+            a = Convert.ToDouble(Console.ReadLine());
+            b = Convert.ToDouble(Console.ReadLine());
+            c = Convert.ToDouble(Console.ReadLine());
+            CalcSq(a, b, c, out S,out P);
+            double S1 = S;
+            Console.WriteLine("Введите длины сторон второго треугольника");
+            a = Convert.ToDouble(Console.ReadLine());
+            b = Convert.ToDouble(Console.ReadLine());
+            c = Convert.ToDouble(Console.ReadLine());
+            CalcSq(a, b, c, out S, out P);
+            double S2 = S;
+            if (S1 > S2)
+            {
+                Console.WriteLine($"Площадь первого треугольника больше {S1:f2} > {S2:f2}");
+            }
+            else if (S1 < S2)
+            {
+                Console.WriteLine($"Площадь второго треугольника больше {S1:f2} < {S2:f2}");
+            }
+            else
+            {
+                Console.WriteLine($"Площади первого и второго равны {S1:f2} = {S2:f2}");
+            }
+            Console.ReadKey();
+        }
+        static void CalcSq (double a, double b, double c, out double S, out double P)
+        {
+
+            P = (a+b+c) / 2;
+            S = Math.Sqrt(P * (P - a) * (P - b) * (P - c));
+        }
+    }
+}
